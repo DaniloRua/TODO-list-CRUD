405fec29a23936366c1a48a0772c2db805a51cc9
Danilopc <sr_danilo@hotmail.com>
2023-08-01 23:13:36 +0100

    update responsive

---
+++ script.js
@@ -0,0 +1,13 @@
+// const createButton = window.document.querySelector(".create-button");
+// createButton.addEventListener('click', (e) => {
+//     console.log('Create button clicked');
+//     e.preventDefault();
+
+// });
+
+// const updateButton = window.document.querySelector(".update-button");
+// updateButton.addEventListener('click', (e) => {
+//     console.log('Update button clicked');
+//     e.preventDefault();
+  
+// });
