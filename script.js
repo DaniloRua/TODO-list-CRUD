5792baf8c16a952cbef2dba9b895ccfad3f0bfd5
Danilopc <sr_danilo@hotmail.com>
2023-08-04 16:23:40 +0100

    add itens on list

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// const createButton = window.document.querySelector(".create-button");
-// createButton.addEventListener('click', (e) => {
-//     console.log('Create button clicked');
-//     e.preventDefault();
-
-// });
 
 // const updateButton = window.document.querySelector(".update-button");
 // updateButton.addEventListener('click', (e) => {
@@ -11,3 +5,23 @@
 //     e.preventDefault();
   
 // });
+
+const form = document.querySelector(".my-form");
+let list = document.querySelector(".toDoList");
+let toDoItem = window.document.querySelector(".form-input")
+
+function createItem (){
+    
+    let item= toDoItem.value;
+    let itemId = String(Date.now());
+    let li = document.createElement('li');
+    li.setAttribute('id',itemId);
+    li.innerText= item;
+    list.appendChild(li);
+    toDoItem.value = '';
+    
+   
+}
+
+
+
