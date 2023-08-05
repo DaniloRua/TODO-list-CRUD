b606974ac1a82ba11425cf4bfa0c30214a8350e2
Danilopc <sr_danilo@hotmail.com>
2023-08-05 21:54:51 +0100

    avoid empty input + layout adjustments

--- script.js
+++ script.js
@@ -3,23 +3,26 @@
 // updateButton.addEventListener('click', (e) => {
 //     console.log('Update button clicked');
 //     e.preventDefault();
-  
+
 // });
 
 const form = document.querySelector(".my-form");
 let list = document.querySelector(".todo-list");
 let toDoItem = window.document.querySelector(".form-input")
 
-function createItem (){
-    
-    let item= toDoItem.value;
-    let itemId = String(Date.now());
-    let li = document.createElement('li');
-    li.setAttribute('id',itemId);
-    li.innerText= item;
-    list.appendChild(li);
-    toDoItem.value = '';
-
+function createItem() {
+    if (toDoItem.value == '') {
+        alert("Enter a valid taks")
+    }
+    else {
+        let item = toDoItem.value;
+        let itemId = String(Date.now());
+        let li = document.createElement('li');
+        li.setAttribute('id', itemId);
+        li.innerText = item;
+        list.appendChild(li);
+        toDoItem.value = '';
+    }
 }
 
 
