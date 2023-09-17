adf7d4836e5cc0dfc0e886f3d8c98e56a5b9cf71
Danilopc <sr_danilo@hotmail.com>
2023-09-17 20:17:17 +0100

    local storage added

--- script.js
+++ script.js
@@ -1,89 +1,76 @@
-
 const form = document.querySelector(".my-form");
 let list = document.querySelector(".todo-list");
 let toDoItem = window.document.querySelector(".form-input");
 const button = document.querySelector(".create-button");
 
+// Load items from local storage when the page loads
+window.addEventListener('load', () => {
+    const savedItems = JSON.parse(localStorage.getItem('todoItems')) || [];
+    savedItems.forEach(item => {
+        createItem(item);
+    });
+});
 
 function clearList() {
-
     if (confirm("Are you sure?")) {
         list.innerHTML = "";
+        localStorage.removeItem('todoItems');
     }
 }
 
 function triggered(e) {
     let element = document.getElementById(e);
     element.classList.toggle("li-toggle");
-    
+    saveItemsToLocalStorage();
 }
-function dblClick (e){
 
-let input = document.createElement('input', Text)
-
-const element = document.getElementById(e);
-console.log(element)
-const elementValue = element.innerHTML;
-
-input.innerText= elementValue;
-
-input.setAttribute('value',elementValue)
-
-console.log(input)
-
-element.replaceWith(input);
-
-input.addEventListener('keypress', (e)=>{
-    
-        if(e.key === 'Enter'){
+function dblClick(e) {
+    let input = document.createElement('input');
+    const element = document.getElementById(e);
+    const elementValue = element.innerHTML;
+    input.value = elementValue;
+    element.replaceWith(input);
+    input.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
             let inputValue = input.value;
-            
             input.remove();
-            element.innerHTML=inputValue;
-            
-            console.log(element);
-
-            return list.appendChild(element);
+            element.innerHTML = inputValue;
+            list.appendChild(element);
+            saveItemsToLocalStorage();
         }
-    
-})
-
- 
+    });
 }
 
-// function createUpdate(e){
-//     let updateButton = document.createElement('button');
-//     updateButton.setAttribute('class',"update-button");
-//     updateButton.setAttribute('id',e);
-    
-//     return updateButton
-// }
-
-function createItem() {
-    if (toDoItem.value == '') {
-        alert("Enter a valid task")
+function createItem(value) {
+    let item = value || toDoItem.value;
+    if (item == '') {
+        alert("Enter a valid task");
+        return;
     }
-    else {
-        let item = toDoItem.value;
-        let itemId = String(Date.now());
-        let li = document.createElement('li');
-        li.setAttribute('id', itemId);
-        li.setAttribute('onclick', "triggered(this.id)");
-        li.setAttribute('ondblclick', "dblClick(this.id)")
-        li.innerText = item;
-        // let updateButton = createUpdate(itemId);
-        // li.appendChild(updateButton)
-        list.appendChild(li);
+
+    let itemId = String(Date.now());
+    let li = document.createElement('li');
+    li.setAttribute('id', itemId);
+    li.setAttribute('onclick', "triggered(this.id)");
+    li.setAttribute('ondblclick', "dblClick(this.id)");
+    li.innerText = item;
+    list.appendChild(li);
+
+    // Clear the input field if a value was provided
+    if (value) {
         toDoItem.value = '';
-
     }
+
+    saveItemsToLocalStorage();
 }
 
+function saveItemsToLocalStorage() {
+    const items = Array.from(list.children).map(li => li.innerText);
+    localStorage.setItem('todoItems', JSON.stringify(items));
+}
 
-
-// button.addEventListener("keypress", function (event) {
-//     if (event.keyCode == 'Enter') {
-//         console.log("rodou")
-//       createItem();
-//     }
-//  });
+toDoItem.addEventListener("keypress", function (event) {
+    if (event.key === 'Enter') {
+        createItem();
+    }
+});
